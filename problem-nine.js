ab2ffe9f9e55203e79e45bb37a1936b6d7159ed4
Daniel Cohen <danielcohen@pvh.com>
2023-06-20 08:50:02 +0100

    added problem nine

---
+++ problem-nine.js
@@ -0,0 +1,13 @@
+const specialPythagoreanTriplet = (n) => {
+  for (let i = 1; i < n / 3; i++) {
+    for (let j = i + 1; j < n / 2; j++) {
+      const k = n - i - j;
+      if (Math.pow(i, 2) + Math.pow(j, 2) === Math.pow(k, 2)) {
+        return i * j * k;
+      }
+    }
+  }
+  return null;
+};
+
+console.log(specialPythagoreanTriplet(1000));
