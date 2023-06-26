a0b36acb5c2ca97535a6806d7ea82d3fd5e98ad7
Daniel Cohen <danielcohen@pvh.com>
2023-06-26 08:23:21 +0100

    changed the names to numbers and added number 15

---
+++ problem-15.js
@@ -0,0 +1,21 @@
+const numOfPathsDP = (n) => {
+  let grid = Array(n + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(0));
+  // Fill the edges with 1
+  for (let i = 0; i <= n; i++) {
+    grid[i][0] = 1;
+    grid[0][i] = 1;
+  }
+
+  // Fill the inside of the grid
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+    }
+  }
+
+  return grid[n][n];
+};
+
+console.log(numOfPathsDP(20));
