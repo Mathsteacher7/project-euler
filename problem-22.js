cd276b1da1ddab9822713d10edf033f28afc6b72
Daniel Cohen <danielcohen@pvh.com>
2023-07-01 08:58:26 +0100

    solved problem 22

---
+++ problem-22.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+
+const alphabetValue = (char) => char.charCodeAt(0) - "A".charCodeAt(0) + 1;
+
+const nameScore = (name, index) => {
+  let sum = 0;
+  for (let char of name) {
+    sum += alphabetValue(char);
+  }
+  return sum * (index + 1);
+};
+
+const calculateTotalScore = async () => {
+  let data = await readFile("./names-problem-twenty-two.txt", "utf8");
+  let names = data
+    .split(",")
+    .map((name) => name.replace(/"/g, ""))
+    .sort();
+  return names.reduce((sum, name, index) => sum + nameScore(name, index), 0);
+};
+
+// Using the function
+calculateTotalScore().then(console.log).catch(console.error);
